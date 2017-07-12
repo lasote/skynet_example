b64d6617a0e4f50aa6da5794218b6ff66abb1590
Luis Martinez de Bartolome <lasote@gmail.com>
2017-07-12 18:46:04 +0300

    C not depending on A

--- recipes/C/conanfile.py
+++ recipes/C/conanfile.py
@@ -8,9 +8,6 @@ class CConan(ConanFile):
     url = "https://github.com/lasote/skynet_example"
     settings = "os", "compiler", "build_type", "arch"
 
-    def requirements(self):
-        self.requires("LIB_A/1.0@%s/%s" % (self.user, self.channel))
-
     def build(self):
         self.output.warn("Building library...")
         time.sleep(2)
